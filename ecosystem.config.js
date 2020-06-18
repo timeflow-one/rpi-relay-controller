2018e4c310a4a1b8205e6b3d7f3ab756da62cae1
nionoku <nionoku@gmail.com>
2020-06-18 22:19:38 +0300

    Add ecosystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'rpi-relay-controller',
+      script: 'src/App.js',
+      interpreter: 'node',
+      interpreter_args: [
+        '-r', 'dotenv/config',
+        '-r', 'ts-node/register',
+        '-r', 'tsconfig-paths/register'
+      ],
+      max_memory_restart: '500M',
+      instances: 1,
+      wait_ready: true,
+      autorestart: true,
+      watch: false
+    }
+  ]
+};
