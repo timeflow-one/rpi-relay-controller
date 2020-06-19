3e9b7afe97fbeef5679669d76c516191e16fe124
nionoku <nionoku@gmail.com>
2020-06-19 15:26:02 +0300

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
-# Question auth basic name
-htpasswd -c nginx/.htpasswd $AUTH_BASIC_NAME
-
-# Get or manual input available_relays.json
+read -p 'Auth basic username: ' auth_basic_name
+htpasswd -c nginx/.htpasswd $auth_basic_name
 
 cp config/available_locks.example.json config/available_locks.json
 nano config/available_locks.json
 
 # Build containers
+docker-compose -f docker-compose.yml build
 # Start containers
+docker-compose -f docker-compose.yml up
