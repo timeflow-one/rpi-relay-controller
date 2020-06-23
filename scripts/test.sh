39fd40a7c50758cc9df8dcff69b1225a1876de9d
nionoku <nionoku@gmail.com>
2020-06-23 22:31:42 +0300

    Remove test connecting script

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-read -p 'IP: ' ip
-read -p 'Auth Basic login: ' auth_login
-read -p 'Auth Basic password: ' auth_pass
-read -p 'Source: ' source
-
-curl -X POST http://$ip/lock/open --basic $auth_login:$auth_pass -H 'Content-Type: application/json' -d '{"source":"$source","initiator":"test"}'
